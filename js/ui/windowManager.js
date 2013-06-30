4aad06e5fa649a284eb2a49c027a6356af6e7437
autarkper <github@autark.se>
2013-06-30 19:12:58 +0200

    [Window Manager] make sure mixing effects for minimizing and mapping work together
    In particular, make sure that the combination of desktop-effects-minimize-effect 'fade'
    and desktop-effects-map-effect 'none' does not leave the restored window invisible.
    
    This also takes care of the case where desktop effects are temporarily turned off when
    restoring a minimized window, which could leave the restored window in a bad state.

--- js/ui/windowManager.js
+++ js/ui/windowManager.js
@@ -226,29 +226,19 @@ WindowManager.prototype = {
     },
 
     _minimizeWindow : function(cinnamonwm, actor) {
+        // reset all cached values in case "traditional" is no longer in effect
+        actor.get_meta_window()._cinnamonwm_has_origin = false;
+        actor.get_meta_window()._cinnamonwm_minimize_transition = undefined;
+        actor.get_meta_window()._cinnamonwm_minimize_time = undefined;
+
         if (!this._shouldAnimate(actor)) {
             cinnamonwm.completed_minimize(actor);
             return;
         }
 
-        let transition = "easeInSine";
-        let effect = "traditional";
-        let time = 0.25;
-        try{
-            effect = global.settings.get_string("desktop-effects-minimize-effect");                                                
-            transition = global.settings.get_string("desktop-effects-minimize-transition");                        
-            time = global.settings.get_int("desktop-effects-minimize-time") / 1000;
-        }
-        catch(e) {
-            log(e);
-        }
-
-        if (actor.get_meta_window()._cinnamonwm_has_origin) {
-            // reset all cached values in case "traditional" is no longer in effect
-            actor.get_meta_window()._cinnamonwm_has_origin = false;
-            actor.get_meta_window()._cinnamonwm_minimize_transition = undefined;
-            actor.get_meta_window()._cinnamonwm_minimize_time = undefined;
-        }
+        let effect = global.settings.get_string("desktop-effects-minimize-effect");
+        let transition = global.settings.get_string("desktop-effects-minimize-transition");
+        let time = global.settings.get_int("desktop-effects-minimize-time") / 1000;
 
         if (effect == "traditional") {
             if (AppletManager.get_role_provider_exists(AppletManager.Roles.WINDOWLIST)) {
@@ -535,7 +525,7 @@ WindowManager.prototype = {
             return;
         }
         if (!this._shouldAnimate(actor)) {
-            cinnamonwm.completed_map(actor);
+            this._completeMap(cinnamonwm, actor);
             return;
         }
         
@@ -544,8 +534,8 @@ WindowManager.prototype = {
         let effect = "scale";
         let time = 0.25;
         try{
-            effect = global.settings.get_string("desktop-effects-map-effect");                                                
-            transition = global.settings.get_string("desktop-effects-map-transition");                        
+            effect = global.settings.get_string("desktop-effects-map-effect");
+            transition = global.settings.get_string("desktop-effects-map-transition");
             time = global.settings.get_int("desktop-effects-map-time") / 1000;
         }
         catch(e) {
@@ -601,30 +591,36 @@ WindowManager.prototype = {
             actor.show();
             this._fadeWindow(cinnamonwm, actor, 255, time, transition, this._mapWindowDone, this._mapWindowOverwrite);
         }
-        else if (effect == "scale") {               
+        else if (effect == "scale") {
             actor.set_scale(0.0, 0.0);
             actor.show();
             this._mapping.push(actor);
             this._scaleWindow(cinnamonwm, actor, 1, 1, time, transition, this._mapWindowDone, this._mapWindowOverwrite);        
         }
         else {   
-            cinnamonwm.completed_map(actor);
+            this._completeMap(cinnamonwm, actor);
         }
         
+    },
+
+    _completeMap : function(cinnamonwm, actor) {
+        actor.move_anchor_point_from_gravity(Clutter.Gravity.NORTH_WEST);
+        actor.set_scale(1.0, 1.0);
+        actor.opacity = 255;
+        actor.show();
+        cinnamonwm.completed_map(actor);
     },
 
     _mapWindowDone : function(cinnamonwm, actor) {
         if (this._removeEffect(this._mapping, actor)) {
             Tweener.removeTweens(actor);
-            actor.move_anchor_point_from_gravity(Clutter.Gravity.NORTH_WEST);
-            actor.opacity = 255;
-            cinnamonwm.completed_map(actor);
+            this._completeMap(cinnamonwm, actor);
         }
     },
 
     _mapWindowOverwrite : function(cinnamonwm, actor) {
         if (this._removeEffect(this._mapping, actor)) {
-            cinnamonwm.completed_map(actor);
+            this._completeMap(cinnamonwm, actor);
         }
     },
 
